f84e60f7be16d524459fdb12dcf4230c6c6e31e2
r47dzt3ch <jeraldjose16@gmail.com>
2026-07-12 00:02:40 +0800

    feat: initialize app with Voyager navigator, Kamel image loading, and MaterialTheme styling

--- shared/src/commonMain/kotlin/com/jpj/presenterproapp/App.kt
+++ shared/src/commonMain/kotlin/com/jpj/presenterproapp/App.kt
@@ -2,15 +2,21 @@ package com.jpj.presenterproapp
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
 import com.jpj.presenterproapp.ui.DiscoveryScreen
+import media.kamel.image.config.KamelConfig
+import media.kamel.image.config.LocalKamelConfig
+import media.kamel.image.config.default
 
 @Composable
 fun App() {
-    MaterialTheme {
-        Navigator(DiscoveryScreen()) { navigator ->
-            SlideTransition(navigator)
+    CompositionLocalProvider(LocalKamelConfig provides KamelConfig.default) {
+        MaterialTheme {
+            Navigator(DiscoveryScreen()) { navigator ->
+                SlideTransition(navigator)
+            }
         }
     }
 }
